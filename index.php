1c0de6771aa96689347c3f6c63f9f06f738b6ce6
Piktoll <114651661+piktoll@users.noreply.github.com>
2022-10-27 18:18:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,30 @@
 <html>
     <head>
         <title><?php echo $title; ?></title>
+        <style>* { box-sizing: border-box; text-align: center; }</style>
     </head>
     <body>
         <input type="number" id="numberIn" />
-        <button onclick="" name="submit"></button>
+        <button onclick="convert();" id="submitBtn"></button>
+        <p id="resultP"></p>
         <script type="text/javascript">
-            let a = "";
+            let input = 0;
+            let result = "";
             const numberIn = document.getElementById("numberIn");
+            const submitBtn = document.getElementById("submitBtn");
+            const resultP = document.getElementById("resultP");
+            
             let dic = [
             ["", "egy", "kettő", "három", "négy", "öt", "hat", "hét",
             "nyolc", "kilenc"],
             ["", "tíz", "húsz", "harminc", "negyven", "ötven",
              "hatvan", "hetven", "nyolcvan", "kilencven"]
             ];
+            
+            function convert() {
+                input = numberIn.value;
+                resultP.innerText = `${input}, azaz ${result}`;
+            }
         </script>
     </body>
 </html>
